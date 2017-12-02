1da3d10ef5f7e551ed679d1707c6943d18cf47d0
Kenneth Korcal <kakorcal@gmail.com>
2017-12-02 12:24:03 -0800

    adding colors

---
+++ project4/colors.js
@@ -0,0 +1,28 @@
+function colors() {
+  return {
+    red: [1,0,0],
+    redA: [1,0,0,1],
+    green: [0,1,0],
+    greenA: [0,1,0,1],
+    blue: [0,0,1],
+    blueA: [0,0,1,1],
+    orange: [0.9,0.4,0.1],
+    orangeA: [.9,0.4,0.1,1],
+    yellow: [1,1,0],
+    yellowA: [1,1,0,1],
+    purple: [0.9,0,0.8],
+    purpleA: [0.9,0,0.8,1],
+    pink: [1,0.5,1],
+    pinkA: [1,0.5,1,1],
+    brown: [0.7,0.4,0],
+    brownA: [0.7,0.4,0,1],
+    skyBlue: [0,1,1],
+    skyBlueA: [0,1,1,1],
+    magenta: [1,0,1],
+    magentaA: [1,0,1,1],
+    black: [0,0,0],
+    blackA: [0,0,0,1],
+    white: [1,1,1],
+    whiteA: [1,1,1,1]
+  };
+}
